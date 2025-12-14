dbfac99bf3f862a778a075de2eb8e9b5c6575b4e
zakaria <zakaria_aitkaddour@outlook.fr>
2025-12-14 17:05:39 +0100

    Update project configuration 2

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from New Test application :)";
+        return "Hello from New Test application Test  Test :)";
     }
 
     @GetMapping("/user")
